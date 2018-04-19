fae20b37e25ec2d96d50d7e644eddb76dda3f1d0
Anirudhayeola <anirudhayeola@outlook.com>
2018-04-19 17:57:25 +0530

    Testing

---
+++ MVC/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using System.Net.Http;
+
+namespace MVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        [HttpGet]
+       public ActionResult Contactlist()
+        {
+            IEnumerable<MvcEmployeeModel> prsnlist;
+            HttpResponseMessage Response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            prsnlist = Response.Content.ReadAsAsync<IEnumerable<MvcEmployeeModel>>().Result;
+            return View(prsnlist);
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if(id == 0)
+                return View(new MvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MvcEmployeeModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(MvcEmployeeModel prn)
+        {
+            // Insert Functionality
+            if (prn.Id == 0)
+            {
+                HttpResponseMessage Response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", prn).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            // Update Functionality
+            else
+            {
+                HttpResponseMessage Response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/" + prn.Id, prn).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Contactlist");
+        }
+
+       
+        public ActionResult Remove(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Contactlist");
+        }   
+    }
+}
---
+++ MVC/Models/MvcEmployeeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class MvcEmployeeModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please Enter First Name")]
+        public string First_Name { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please Enter Last Name")]
+        public string Last_Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Email")]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}",ErrorMessage = "Please Enter Valid Email")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Please Enter Phone Number")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",ErrorMessage = "Please Enter Valid Phone Number")]
+        public string Phone_Number { get; set; }
+        [Required(ErrorMessage = "Please Select Status")]
+        public string Status { get; set; }
+    }
+}
